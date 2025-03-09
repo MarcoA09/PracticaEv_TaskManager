1cc4ba154a308a1ddb7a67fc64c6020a7b3235cf
MarcoA09 <marcoa.marquez06@gmail.com>
2025-03-08 19:01:20 -0600

    Update jwt.js

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -1,5 +1,6 @@
-import { TOKEN_SECRET } from "../config.js";
 import jwt from "jsonwebtoken";
+
+const TOKEN_SECRET = process.env.TOKEN_SECRET
 
 export async function createAccessToken(payload) {
   return new Promise((resolve, reject) => {
